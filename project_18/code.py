f2f80ed2ee43f039edd4ae1537144f9ddee50e6a
KhushalPatel <khushalt.aipl@gmail.com>
2026-04-23 09:37:51 +0530

    initial commit

---
+++ project_18/code.py
@@ -0,0 +1,16 @@
+import random
+import math
+import os
+
+
+print((random.random()*1000))
+
+
+print(math.trunc(random.random()*1000))
+
+random.seed(10)
+
+print(random.random())
+
+
+print(os.listdir("/Users/khushaltaraviya/Documents/Red_and_White_Projects/"))
---
+++ project_19/code.py
@@ -0,0 +1,27 @@
+
+# import entire module
+# import module
+
+
+#import specific items from module
+from module import add, greet
+
+
+#renaming module
+
+# import module as mm
+
+# res = mm.add(10,10)
+# print(res)
+# mm.greet('Khushal')
+
+
+# res = module.add(10,10)
+
+res = add(10,10)
+
+print(res)
+
+# module.greet('Khushal')
+
+greet('Khushal')
